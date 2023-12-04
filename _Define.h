982abab473d49ca44d2fcc73b02d2fd0fd8957da
UnlimitedAbandonWare <nninni79@naver.com>
2023-12-04 18:35:16 +0900

    Add files via upload

---
+++ _Define.h
@@ -0,0 +1,10 @@
+#pragma once
+#define DLL_FUNC static unsigned long __stdcall
+
+#define STDCALL_FUNC unsigned long __stdcall
+
+#define MAIN_STACK_SIZE 4096 * 24 //메인 컨트롤 타워 쓰레드 사이즈
+
+#define PCH_BIG_POINTER_SIZE 0x401000 // 예시 크기
+
+
